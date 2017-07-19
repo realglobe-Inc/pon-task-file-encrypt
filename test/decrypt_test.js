7985ecc0c28490d89db73d98cb4cadbfb85bd4b5
FujiHaruka <fujiharuka@realglobe.jp>
2017-07-19 16:16:17 +0900

    v1

---
+++ test/decrypt_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for decrypt.
+ * Runs with mocha.
+ */
+'use strict'
+
+const decrypt = require('../lib/decrypt.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('decrypt', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Decrypt', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
